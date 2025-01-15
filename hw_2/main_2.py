ae935065c865b82c46fe137cbf7d831daa522e1b
Григорьян Тигран Фридрихович <tf.grigoryan@voenet.local>
2025-01-15 10:26:47 +0300

    Begin hw_2

---
+++ hw_2/main_2.py
@@ -0,0 +1,10 @@
+#Задача_1
+
+class Patient:
+
+    def __init__(self, name: str, age: int, disease: str):
+        self.name = name
+        self.age = age
+        self.disease = disease
+
+    def appointment(self, ):
